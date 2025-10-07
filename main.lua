23df50c02d11b3c9a49e9a55a41f8c0241770a77
Serg <serhiipoklonsky2@gmail.com>
2025-10-06 21:56:46 -0400

    v1

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 -- Serhii X (with actual comments so you know what the script does)
 
 -- Load WindUI
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+-- Make window
